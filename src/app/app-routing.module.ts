02332c0134a28dcff0fa318171f72f6ddf09077d
aghariebiyan <alirezaa_afg@live.nl>
2020-01-14 15:12:19 +0100

    navbar verder uitgewerkt voor de Cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductUpdateComponent } from './admin/product/product-update/product-update.component';
-import { ProductComponent } from './admin/product/product.component';
 import {ProductCustomerComponent} from './customer/product-customer/product-customer.component';
 import {ProductAddComponent} from './admin/product/product-add/product-add.component';
 import {ContactComponent} from './contact/contact.component';
 import {AboutUsComponent} from './about-us/about-us.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './login/register/register.component';
+import { AdminComponent } from './admin/admin.component';
+import {CartComponent} from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
@@ -16,9 +17,10 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent},
   { path: 'contact', component: ContactComponent },
   { path: 'about-us', component: AboutUsComponent },
-  { path: 'admin/products', component: ProductComponent },
+  { path: 'admin/products', component: AdminComponent },
   { path: 'admin/product/add', component: ProductAddComponent },
-  { path: 'admin/update/:productId', component: ProductUpdateComponent }
+  { path: 'admin/update/:productId', component: ProductUpdateComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
